6ac31d24df2564e3c7a1268eacd748e2c45ee57b
jhtanjim <jh.tanjim.4@gmail.com>
2025-07-16 13:29:45 -0700

    done

--- src/Pages/DashBoard/Payment/CheckOutForm.jsx
+++ src/Pages/DashBoard/Payment/CheckOutForm.jsx
@@ -4,7 +4,6 @@ import { useContext, useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { AuthContext } from "../../../Providers/AuthProvider";
-// import "./CheckoutForm.css";
 
 const CheckoutForm = ({ cart, price }) => {
   const stripe = useStripe();
