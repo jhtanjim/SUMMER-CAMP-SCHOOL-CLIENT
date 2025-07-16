fd96c8a11b1fdbdd0f35fefaed73a46a1a1c4114
jhtanjim <jh.tanjim.4@gmail.com>
2025-07-16 13:24:37 -0700

    done

--- src/Pages/Shared/FourOfour/FourOfour.jsx
+++ src/Pages/Shared/FourOfour/FourOfour.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const FourOfour = () => {
-    return (
-        <div>
-            <div>
-                <div className="not-found text-center">
-                    <img className="w-25" src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-1932.jpg?size=626&ext=jpg&ga=GA1.2.2105197794.1682091372&semt=ais" alt="404" />
-                    <p>Oops! The page you're looking for does not exist.</p>
-                    <Link to="/" className="btn btn-primary">
-                        Back to Home
-                    </Link>
-                </div>
-            </div>
-        </div>
-    );
+const UnderConstruction = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-yellow-100 px-4">
+      <div className="text-center">
+        <img
+          className="w-64 mx-auto mb-6"
+          src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-1932.jpg?size=626&ext=jpg&ga=GA1.2.2105197794.1682091372&semt=ais"
+          alt="404 - Not Found"
+        />
+        <h1 className="text-3xl font-bold text-orange-600 mb-2">
+          Uh-oh! Page Not Found
+        </h1>
+        <p className="text-gray-700 mb-4">
+          It looks like you wandered off the summer camp trail. Let’s head back!
+        </p>
+        <Link
+          to="/"
+          className="inline-block px-6 py-2 text-white bg-orange-500 hover:bg-orange-600 rounded-full transition"
+        >
+          Back to Camp Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default FourOfour;
+export default UnderConstruction;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,97 +1,104 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import DashBoard from "../Layout/DashBoard";
+import Main from "../Layout/Main";
+import MySelectedClass from "../Pages/DashBoard/MySelectedClass/MySelectedClass";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
-import SignUp from "../Pages/SignUp/SignUp";
-import FourOfour from "../Pages/Shared/fourOfour/fourOfour";
 import Classes from "../Pages/Shared/Classes/Classes";
-import Main from "../Layout/Main";
 import Instuctors from "../Pages/Shared/Instuctors/Instuctors";
-import DashBoard from "../Layout/DashBoard";
-import MySelectedClass from "../Pages/DashBoard/MySelectedClass/MySelectedClass";
+import SignUp from "../Pages/SignUp/SignUp";
 import PrivateRoutes from "./PrivateRoutes";
 
-import ManageUsers from "../Pages/DashBoard/ManageUsers/ManageUsers";
 import AddClass from "../Pages/DashBoard/AddClass/AddClass";
-import AdminRoute from "./AdminRoute";
-import MyClasses from "../Pages/DashBoard/MyClasses/MyClasses";
 import ManageClasses from "../Pages/DashBoard/ManageClasses/ManageClasses";
-import Payment from "../Pages/DashBoard/Payment/Payment";
+import ManageUsers from "../Pages/DashBoard/ManageUsers/ManageUsers";
+import MyClasses from "../Pages/DashBoard/MyClasses/MyClasses";
 import MyEnrolledClass from "../Pages/DashBoard/MyEnrolledClass/MyEnrolledClass";
+import Payment from "../Pages/DashBoard/Payment/Payment";
 import PaymentHistory from "../Pages/DashBoard/PaymentHistory/PaymentHistory";
+import UnderConstruction from "../Pages/Shared/FourOfour/FourOfour";
+import AdminRoute from "./AdminRoute";
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
         path: "/",
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/login',
-                element: <Login></Login>
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signUp",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/classes",
+        element: <Classes></Classes>,
+      },
+      {
+        path: "/instuctors",
+        element: <Instuctors></Instuctors>,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoutes>
+        <DashBoard></DashBoard>
+      </PrivateRoutes>
+    ),
+    children: [
+      {
+        path: "MySelectedClass",
+        element: <MySelectedClass></MySelectedClass>,
+      },
 
-            },
-            {
-                path: '/signUp',
-                element: <SignUp></SignUp>
-            },
-            {
-                path: '/classes',
-                element: <Classes></Classes>
-            },
-            {
-                path: '/instuctors',
-                element: <Instuctors></Instuctors>
-            },
+      {
+        path: "payment",
+        element: <Payment></Payment>,
+      },
+      {
+        path: "PaymentHistory",
+        element: <PaymentHistory></PaymentHistory>,
+      },
+      {
+        path: "MyEnrolledClass",
+        element: <MyEnrolledClass></MyEnrolledClass>,
+      },
 
-        ],
-
-    },
-    {
-        path: 'dashboard',
-        element: <PrivateRoutes><DashBoard></DashBoard></PrivateRoutes>,
-        children: [
-            {
-                path: 'MySelectedClass',
-                element: <MySelectedClass></MySelectedClass>
-            },
-
-            {
-                path: 'payment',
-                element: <Payment></Payment>
-            },
-            {
-                path: 'PaymentHistory',
-                element: <PaymentHistory></PaymentHistory>
-            },
-            {
-                path: 'MyEnrolledClass',
-                element: <MyEnrolledClass></MyEnrolledClass>
-            },
-
-            {
-                path: 'manageUsers',
-                element: <AdminRoute><ManageUsers></ManageUsers></AdminRoute>
-            },
-            {
-                path: 'addClass',
-                element: <AddClass></AddClass>
-            },
-            {
-                path: 'myclasses',
-                element: <MyClasses></MyClasses>
-            },
-            {
-                path: 'manageClasses',
-                element: <AdminRoute><ManageClasses></ManageClasses></AdminRoute>
-            },
-        ],
-    },
-    {
-        path: '*',
-        element: <FourOfour></FourOfour>
-    },
+      {
+        path: "manageUsers",
+        element: (
+          <AdminRoute>
+            <ManageUsers></ManageUsers>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "addClass",
+        element: <AddClass></AddClass>,
+      },
+      {
+        path: "myclasses",
+        element: <MyClasses></MyClasses>,
+      },
+      {
+        path: "manageClasses",
+        element: (
+          <AdminRoute>
+            <ManageClasses></ManageClasses>
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <UnderConstruction />,
+  },
 ]);
